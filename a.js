6bd4434099422ec513997631f78d5de91c2f71fb
oktaykaypak <oktay_07_89@windowslive.com>
2021-03-29 15:39:24 +0300

    Initial commit

---
+++ a.js
@@ -0,0 +1,15 @@
+const mongodb = require('mongodb');
+
+name()
+
+async function name() {
+    const client = await mongodb.MongoClient.connect(
+        'mongodb://root:1@94.130.225.52:27017/', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+    );
+    var a = await client.db('orendapush').collection('bebeklikdata').find({ _id:"fQFWYVYpSoI1EgWEZSNEuv:APA91bGuCI1hLf2aK076whVBPk-dWB75CDhfVtfgpSbyX7K3ZOGeuqMVV0kLWj7T_LPK1qv2S_oIgalYDGxzPcGgzYAR3v2KtfLqtt69l9pPYzMF6xqnY8gOcdVNJDXYvX1PWe53RIzg" }).toArray()
+    console.log(a);
+
+}
